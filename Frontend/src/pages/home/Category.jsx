7072645031a65188e05db053cd62ae5473db5a82
DeepakS-Github <deepakanonymous75@gmail.com>
2023-12-21 16:13:12 +0530

    Images are fixed in the home page

--- Frontend/src/pages/home/Category.jsx
+++ Frontend/src/pages/home/Category.jsx
@@ -8,49 +8,49 @@ function Category() {
       <Link to={"/category/rice"}>
         <CategoryCard
           title="Rice"
-          image="https://source.unsplash.com/random/?rice"
+          image="https://source.unsplash.com/xmuIgjuQG0M"
         />
       </Link>
       <Link to={"/category/wheat"}>
         <CategoryCard
           title="Wheat"
-          image="https://source.unsplash.com/random/?wheat"
+          image="https://source.unsplash.com/joOVC9d-jis"
         />
       </Link>
       <Link to={"/category/nuts"}>
         <CategoryCard
           title="Nuts"
-          image="https://source.unsplash.com/random/?nuts"
+          image="https://source.unsplash.com/UhrHTmVBzzE"
         />
       </Link>
       <Link to={"/category/sugar"}>
         <CategoryCard
           title="Sugar"
-          image="https://source.unsplash.com/random/?sugar"
+          image="https://source.unsplash.com/u_Mwofs_zu0"
         />
       </Link>
       <Link to={"/category/spices"}>
         <CategoryCard
           title="Spices"
-          image="https://source.unsplash.com/random/?spices"
+          image="https://source.unsplash.com/uaHShoIDGeo"
         />
       </Link>
       <Link to={"/category/fruits"}>
         <CategoryCard
           title="Fruits"
-          image="https://source.unsplash.com/random/?fruits"
+          image="https://source.unsplash.com/M_xIaxQE3Ms"
         />
       </Link>
       <Link to={"/category/vegetables"}>
         <CategoryCard
           title="Vegetables"
-          image="https://source.unsplash.com/random/?vegetables"
+          image="https://source.unsplash.com/5aJVJvJ9rG8"
         />
       </Link>
       <Link to={"/category/pulses"}>
         <CategoryCard
           title="Pulses"
-          image="https://source.unsplash.com/random/?pulses"
+          image="https://source.unsplash.com/TUf3H3vRlNU"
         />
       </Link>
     </div>
