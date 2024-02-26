71716ce26a9287f18e679ca03316d025874dea5e
DeepakS-Github <deepakanonymous75@gmail.com>
2024-02-26 23:50:32 +0530

    removed the mailgen code from the backend while sending mail

--- Backend/controllers/mailController.js
+++ Backend/controllers/mailController.js
@@ -16,38 +16,38 @@ const sendMail = (receiverEmailAddress, content, userName, subject) => {
 
     let transporter = nodemailer.createTransport(config);
 
-    let MailGenerator = new Mailgen({
-        theme: "default",
-        product : {
-            name: "CropConnect",
-            link : 'https://mailgen.js/'
-        }
-    })
+    // let MailGenerator = new Mailgen({
+    //     theme: "default",
+    //     product : {
+    //         name: "CropConnect",
+    //         link : 'https://mailgen.js/'
+    //     }
+    // })
 
-    let response = {
-        body: {
-            name : userName,
-            intro: content,
-            // table : {
-            //     data : [
-            //         {
-            //             item : "Nodemailer Stack Book",
-            //             description: "A Backend application",
-            //             price : "$10.99",
-            //         }
-            //     ]
-            // },
-            outro: "Looking forward to do more business"
-        }
-    }
+    // let response = {
+    //     body: {
+    //         name : userName,
+    //         intro: content,
+    //         // table : {
+    //         //     data : [
+    //         //         {
+    //         //             item : "Nodemailer Stack Book",
+    //         //             description: "A Backend application",
+    //         //             price : "$10.99",
+    //         //         }
+    //         //     ]
+    //         // },
+    //         outro: "Looking forward to do more business"
+    //     }
+    // }
 
-    let mail = MailGenerator.generate(response)
+    // let mail = MailGenerator.generate(response)
 
     let message = {
         from : process.env.EMAIL,
         to : userEmail,
         subject: subject,
-        html: mail
+        html: content
     }
     transporter.sendMail(message).then(() => {
         console.log("Mail sent");
