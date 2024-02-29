d0545bec49eea9ed556be350cc95f3fb3867b054
DeepakS-Github <deepakanonymous75@gmail.com>
2024-02-29 22:52:45 +0530

    Changed the bg color of skeleton loader

--- Frontend/src/components/skeleton/BoxSkeleton.jsx
+++ Frontend/src/components/skeleton/BoxSkeleton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function BoxSkeleton({ height, width, paddingX, paddingY, radius}) {
   return (
     <div
-      className={`bg-gray-200 ${radius} ${paddingX} ${paddingY} ${height} ${width} animate-pulse`}
+      className={`bg-gray-300 ${radius} ${paddingX} ${paddingY} ${height} ${width} animate-pulse`}
     ></div>
   );
 }
