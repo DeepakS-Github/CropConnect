9cfd1ed8528886ec4a5b029a3be5ebd9164c6ae4
DeepakS-Github <deepakanonymous75@gmail.com>
2024-03-01 14:42:03 +0530

    Removes the added dummy text in ProductDetails.jsx

--- Frontend/src/pages/product_details/ProductDetails.jsx
+++ Frontend/src/pages/product_details/ProductDetails.jsx
@@ -56,16 +56,7 @@ function ProductDetails() {
           </h2>
           <Heading text={productData.name} marginY="mb-2" textAlign="left" paddingX="p-0"/>
           <p className="leading-relaxed text-sm md:text-base">
-            {productData.description} It is a long established fact that a
-            reader will be distracted by the readable content of a page when
-            looking at its layout. The point of using Lorem Ipsum is that it has
-            a more-or-less normal distribution of letters, as opposed to using
-            'Content here, content here', making it look like readable English.
-            Many desktop publishing packages and web page editors now use Lorem
-            Ipsum as their default model text, and a search for 'lorem ipsum'
-            will uncover many web sites still in their infancy. Various versions
-            have evolved over the years, sometimes by accident, sometimes on
-            purpose (injected humour and the like).
+            {productData.description}
           </p>
 
           <div className="relative overflow-x-auto my-6">
