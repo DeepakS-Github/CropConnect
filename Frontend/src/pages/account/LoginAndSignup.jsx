cf2de042f4a214ca65ebb05b9c2bbb06fd76f177
DeepakS-Github <deepakanonymous75@gmail.com>
2024-03-01 15:50:12 +0530

    updated the ring color of the brand name in signup seller section

--- Frontend/src/pages/account/LoginAndSignup.jsx
+++ Frontend/src/pages/account/LoginAndSignup.jsx
@@ -200,7 +200,7 @@ function LoginAndSignup() {
                         {...(!signIn ? { required: true } : {})}
                         value={brandName}
                         onChange={(e) => setBrandName(e.target.value)}
-                        className={`block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6`}
+                        className={`block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6`}
                       />
                     </div>
                   </div>
