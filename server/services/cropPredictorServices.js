ff0655fb13b44b837dede69ab08bbe20dd52b719
DeepakS-Github <deepakanonymous75@gmail.com>
2024-04-09 02:46:29 +0530

    folder name change from Frontend to client and Backend to server

--- server/services/cropPredictorServices.js
+++ server/services/cropPredictorServices.js
@@ -7,7 +7,7 @@ async function cropPredictorServices(soil, altitude, temperature, humidiy, rainf
 
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-
+  
   const prompt =
     `Predict the crops and give me data based on these environmental factors: Soil type: ${soil} Altitude (in km): ${altitude} Temperature (in degree Celsius): ${temperature} Humidity (in %): ${humidiy} Rainfall (in mm): ${rainfall} Note: Ensure the following conditions are met: - Altitude should be a numerical value between 0 and 10 (kilometers). - Temperature should be a numerical value between -50 and 50 (degree Celsius). - Humidity should be a numerical value between 0 and 100 (%). - Rainfall should be a numerical value between 0 and 1000 (mm).`;
 
