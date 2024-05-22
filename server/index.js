32bd93d5813edbeeaaa522d9fbe63af4c77b3599
DeepakS-Github <deepakanonymous75@gmail.com>
2024-05-22 13:17:50 +0530

    add: dev frontend deployed url cors access

--- server/index.js
+++ server/index.js
@@ -11,19 +11,23 @@ const product = require("./routes/product");
 const review = require("./routes/review");
 const order = require("./routes/order");
 const faq = require("./routes/faq");
-const graph = require("./routes/graph.js")
-const ai = require("./routes/ai.js")
+const graph = require("./routes/graph.js");
+const ai = require("./routes/ai.js");
 const auth = require("./routes/auth");
 
 const PORT = 8080;
 const app = express();
 
-app.use(cors(
-  {
-    origin: ["https://localhost:5173", "https://crop-connect-lime.vercel.app"],
-    credentials: true
-  }
-));
+app.use(
+  cors({
+    origin: [
+      "https://localhost:5173",
+      "https://crop-connect-lime.vercel.app",
+      "https://crop-connect-git-dev-deepaksgithubs-projects.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -45,7 +49,6 @@ app.use("/order", order);
 app.use("/faqs", faq);
 app.use("/graph", graph);
 app.use("/ai", ai);
-
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
