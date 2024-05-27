4363ee1cb9a14267ed68859018c224f2674403b2
DeepakS-Github <deepakanonymous75@gmail.com>
2024-05-27 15:29:06 +0530

    add: enabled maxAge in cookie

--- server/services/cookieServices.js
+++ server/services/cookieServices.js
@@ -1,8 +1,10 @@
 const setCookie = (res, name, value) => {
   const oneDayMilliseconds = 24 * 60 * 60 * 1000;
 
+  console.log("Setting cookie", name, value);
+
   res.cookie(name, value, {
-    // maxAge: oneDayMilliseconds, // oneDayMilliseconds from now
+    maxAge: oneDayMilliseconds, // oneDayMilliseconds from now
     httpOnly: false,
     sameSite: "none",
     secure: true,
