0c32afa41cf1f7297e954c32b39e85f6b3849e67
DeepakS-Github <deepakanonymous75@gmail.com>
2024-05-27 15:05:04 +0530

    rem: temporary removed the maxAge

--- server/services/cookieServices.js
+++ server/services/cookieServices.js
@@ -2,7 +2,7 @@ const setCookie = (res, name, value) => {
   const oneDayMilliseconds = 24 * 60 * 60 * 1000;
 
   res.cookie(name, value, {
-    maxAge: oneDayMilliseconds, // oneDayMilliseconds from now
+    // maxAge: oneDayMilliseconds, // oneDayMilliseconds from now
     httpOnly: false,
     sameSite: "none",
     secure: true,
