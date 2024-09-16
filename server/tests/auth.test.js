7d87aa3487bad616b3c354c1b03b11a26221a158
DeepakS-Github <21310@iiitu.ac.in>
2024-09-16 12:39:39 +0530

    complete: auth server tests

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -4,7 +4,6 @@ const request = require('supertest');
 jest.mock('../services/mailServices');
 
 const sendMail = require('../services/mailServices');
-const extractVerificationTokenAndTypeFromLink = require('../helper/extractVerificationTokenAndTypeFromLink');
 const capitalizeFirstLetter = require('../helper/capitalizeFirstLetter');
 const UserModal = require('../models/userSchema');
 const SellerModal = require('../models/sellerSchema');
@@ -27,24 +26,14 @@ const userDetails = {
 }
 
 
-const userLoginDetails = {
-    email: "user@test.com",
-    password: "test123"
-}
-
-const sellerLoginDetails = {
-    email: "seller@test.com",
-    password: "test123"
-}
-
 
 let user;
 let seller;
 
 
-sendMail.mockImplementation((receiverEmailAddress, content, subject) => {
-    return Promise.resolve();
-});
+// sendMail.mockImplementation((receiverEmailAddress, content, subject) => {
+//     return Promise.resolve();
+// });
 
 
 // Signup tests
@@ -132,11 +121,6 @@ const runSignupTests = (type) => {
 }
 
 
-runSignupTests('seller');
-runSignupTests('user');
-runSignupTests('incorrectType');
-
-
 // Verify tests
 
 const runVerifyTests = (type) => {
@@ -202,7 +186,7 @@ const runVerifyTests = (type) => {
                         await SellerModal.findByIdAndUpdate(seller._id, { verificationTokenExpiry: Date.now() - 1000, isVerified: false });
                     }
                     else {
-                        await UserModal.findByIdAndUpdate(user._id, { verificationTokenExpiry: Date.now() - 1000, isVerified: false  });
+                        await UserModal.findByIdAndUpdate(user._id, { verificationTokenExpiry: Date.now() - 1000, isVerified: false });
                     }
 
                     const res = await request(app).patch(`/auth/${type}/verify/${verificationToken}`);
@@ -216,19 +200,13 @@ const runVerifyTests = (type) => {
 
 }
 
-runVerifyTests('seller');
-// runVerifyTests('user');
-// runVerifyTests('incorrectType');
-
-
-
 
 // Login tests
 
 const runLoginTests = (type) => {
 
     type = type.toLowerCase();
-    const loginDetails = type === 'seller' ? sellerLoginDetails : userLoginDetails;
+    const loginDetails = type === 'seller' ? { email: sellerDetails.email, password: sellerDetails.password } : { email: userDetails.email, password: userDetails.password };
 
     describe(`${capitalizeFirstLetter(type)} Login`, () => {
 
@@ -243,59 +221,67 @@ const runLoginTests = (type) => {
         }
 
         if (type === 'seller' || type === 'user') {
+
             describe(`given the correct ${type} details`, () => {
                 it('should return 200 OK and success payload', async () => {
+
+
+                    if (type === 'seller') {
+                        await SellerModal.findByIdAndUpdate(seller._id, { isVerified: true });
+                    }
+                    else {
+                        await UserModal.findByIdAndUpdate(user._id, { isVerified: true });
+                    }
+
+
                     const res = await request(app).post(`/auth/${type}/login`).send(loginDetails);
 
                     expect(res.statusCode).toEqual(200);
                     expect(res.body).toHaveProperty('message', `${capitalizeFirstLetter(type)} login successful`);
-                    expect(res.body).toHaveProperty('cookies');
+                    expect(res.body).toHaveProperty(`cookies.${type}_access_token`);
                     if (type === 'seller') {
-                        expect(res.body).toHaveProperty('brandName');
+                        expect(res.body).toHaveProperty('cookies.brandName');
                     }
                 })
             })
 
 
-            // describe(`given the repeated ${type} email`, () => {
-            //     it('should return 400 and error message', async () => {
-            //         const modifiedSignupDetails = { ...signupDetails, contact: 9839284392 };
+            describe(`given the ${type} does not exists`, () => {
+                it('should return 404 and error message', async () => {
+                    const res = await request(app).post(`/auth/${type}/login`).send({ ...loginDetails, email: "notfound@test.com" });
 
-            //         if (type === 'seller') {
-            //             modifiedSignupDetails.brandName = "testBrand2";
-            //         }
+                    expect(res.statusCode).toEqual(404);
+                    expect(res.body).toHaveProperty('message', `${capitalizeFirstLetter(type)} not found`);
 
-            //         const res = await request(app).post(`/auth/${type}/login`).send(modifiedSignupDetails);
-            //         expect(res.statusCode).toEqual(400);
-            //         expect(res.body).toHaveProperty('message', `${capitalizeFirstLetter(type)} with this email already exists`);
-            //     })
-            // })
+                })
+            })
 
 
-            // describe(`given the repeated ${type} contact`, () => {
-            //     it('should return 400 and error message', async () => {
-            //         const modifiedSignupDetails = { ...signupDetails, email: "test@test.com" };
-
-            //         if (type === 'seller') {
-            //             modifiedSignupDetails.brandName = "testBrand3";
-            //         }
-
-            //         const res = await request(app).post(`/auth/${type}/login`).send(modifiedSignupDetails);
-            //         expect(res.statusCode).toEqual(400);
-            //         expect(res.body).toHaveProperty('message', `${capitalizeFirstLetter(type)} with this phone number already exists`);
-            //     })
-            // })
+            describe(`given the incorrect ${type} password`, () => {
+                it('should return 401 and error message', async () => {
+                    const res = await request(app).post(`/auth/${type}/login`).send({ ...loginDetails, password: "incorrectPassword" });
+                    expect(res.statusCode).toEqual(401);
+                    expect(res.body).toHaveProperty('message', 'Incorrect password');
+                })
+            })
 
 
-            // if (type === "seller") {
-            //     describe('given the repeated seller brand name', () => {
-            //         it('should return 409 and error message', async () => {
-            //             const res = await request(app).post('/auth/seller/login').send({ ...sellerDetails, email: "nvccafpbxsnudwfaae@ytnhy.com", contact: 1234567899 });
-            //             expect(res.statusCode).toEqual(409);
-            //             expect(res.body).toHaveProperty('message', 'This brand name already exists');
-            //         })
-            //     })
-            // }
+            describe(`given the unverified ${type} account`, () => {
+                it('should return 200 and error message', async () => {
+                    if (type === 'seller') {
+                        await SellerModal.findByIdAndUpdate(seller._id, { isVerified: false });
+                    }
+                    else {
+                        await UserModal.findByIdAndUpdate(user._id, { isVerified: false });
+                    }
+
+
+                    const res = await request(app).post(`/auth/${type}/login`).send(loginDetails);
+                    expect(res.statusCode).toEqual(200);
+                    expect(res.body).toHaveProperty('message');
+                })
+            })
+
         }
 
     })
@@ -303,6 +289,19 @@ const runLoginTests = (type) => {
 }
 
 
-// runLoginTests('seller');
-// runLoginTests('user');
-// runLoginTests('incorrectType');
+// Running seller integration tests
+runSignupTests('seller');
+runVerifyTests('seller');
+runLoginTests('seller');
+
+
+// Running user integration tests
+runSignupTests('user');
+runVerifyTests('user');
+runLoginTests('user');
+
+
+// Running invalid type tests
+runSignupTests('invalid');
+runVerifyTests('invalid');
+runLoginTests('invalid');
