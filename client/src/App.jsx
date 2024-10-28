2319b99377f95fe952ab73c50648d6ba273379f1
DeepakS-Github <21310@iiitu.ac.in>
2024-10-28 20:25:32 +0530

    fix: the default route in seller dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 // import './App.css'
 import React, { useEffect, useMemo } from "react";
 import LoginAndSignup from "./pages/account";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/products";
 import Navbar from "./components/navbar/Navbar";
 import SellerDashboard from "./pages/seller_dashboard";
@@ -28,62 +28,62 @@ function App() {
         <ScrollToTop />
         <Navbar />
         {/* <div className="min-h-[calc(100vh-120px)] md:min-h-[calc(100vh-50px)]"> */}
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/account/:type" element={<LoginAndSignup />} />
-            <Route exact path="/:type/verify/:token" element={<Verify />} />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/account/:type" element={<LoginAndSignup />} />
+          <Route exact path="/:type/verify/:token" element={<Verify />} />
+          <Route
+            exact
+            path="/sellerdashboard"
+            element={<SellerDashboard />}
+          >
+            <Route index element={<Navigate to="overview" replace />} />
             <Route
               exact
-              path="/sellerdashboard"
-              element={<SellerDashboard />}
-            >
-              <Route
-                exact
-                index
-                path="overview"
-                element={<SellerOverview />}
-              />
-              <Route
-                exact
-                path="products"
-                element={<SellerProducts />}
-              />
-              <Route
-                exact
-                path="orders"
-                element={<SellerOrderRequests />}
-              />
-              <Route
-                exact
-                path="faqs"
-                element={<SellerFAQs />}
-              />
-              <Route
-                exact
-                path="cropsense-ai"
-                element={<CropSenseAI/>}
-              />
-            </Route>
-            
-            <Route
-              exact
-              path="/map/:latitude/:longitude"
-              element={<ShowMap />}
+              path="overview"
+              element={<SellerOverview />}
             />
             <Route
               exact
-              path="/sellerdashboard/product/:operation"
-              element={<SellerProductOperation />}
+              path="products"
+              element={<SellerProducts />}
             />
-            <Route exact path="/category/:type" element={<Product />} />
             <Route
               exact
-              path="/category/:type/details/:productId"
-              element={<ProductDashboard />}
+              path="orders"
+              element={<SellerOrderRequests />}
             />
-            <Route exact path="/orders" element={<Order />} />
-            <Route exact path="/map" element={<LeafletMap />} />
-          </Routes>
+            <Route
+              exact
+              path="faqs"
+              element={<SellerFAQs />}
+            />
+            <Route
+              exact
+              path="cropsense-ai"
+              element={<CropSenseAI />}
+            />
+          </Route>
+
+          <Route
+            exact
+            path="/map/:latitude/:longitude"
+            element={<ShowMap />}
+          />
+          <Route
+            exact
+            path="/sellerdashboard/product/:operation"
+            element={<SellerProductOperation />}
+          />
+          <Route exact path="/category/:type" element={<Product />} />
+          <Route
+            exact
+            path="/category/:type/details/:productId"
+            element={<ProductDashboard />}
+          />
+          <Route exact path="/orders" element={<Order />} />
+          <Route exact path="/map" element={<LeafletMap />} />
+        </Routes>
         {/* </div> */}
       </Router>
       <Footer />
